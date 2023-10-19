0a45aad8843f2cc2fc0fe0a052d9e6a872a66261
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2023-10-20 00:49:53 +0530

    updated home

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Web Development",
+          "App Development",
+          "UI/UX Designing",
+          "Search Engine Optimization",
         ],
         autoStart: true,
         loop: true,
