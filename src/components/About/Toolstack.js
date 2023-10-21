0e41ec00caa0b7d85b43043418eab8d0c2fbc636
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2023-10-22 00:25:02 +0530

    updated service tiles

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -16,20 +16,20 @@ import {CgFigma} from "react-icons/cg";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px", }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={4} className="tech-icons">
         <BsGlobe2 />
         <br />
         <p style={{ fontSize: "0.25em", paddingBottom: "10px" }}>Web <br/> Development</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={4} className="tech-icons">
         <BsAndroid />
         <p style={{ fontSize: "0.25em", paddingBottom: "20px" }}>App <br/>Development</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={4} className="tech-icons">
         <FaRobot />
         <p style={{ fontSize: "0.25em", paddingBottom: "20px" }}>AI & ML <br/> Development</p>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={4} className="tech-icons">
         <CgFigma />
         <p style={{ fontSize: "0.25em", paddingBottom: "20px" }}>UI/UX <br/> Designing</p>
       </Col>
